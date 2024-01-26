797f13ebc2470d8a3a73e25aaf91211c1868ce6c
Joseph MANZI <mjos12002@gmail.com>
2024-01-26 17:08:50 +0200

    Inclusion of the security

---
+++ src/main/java/com/twofifty/datasolutionapi/config/ApiSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.twofifty.datasolutionapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ApiSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .authorizeHttpRequests((requests) -> {
+                    requests.requestMatchers("/api/*").permitAll()
+                            .anyRequest().fullyAuthenticated();
+                });
+        return httpSecurity.build();
+    }
+}
